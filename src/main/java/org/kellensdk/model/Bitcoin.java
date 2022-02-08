4e8700d037cfb5d29fe7262e5c34d5a6fb5047a1
Kellen <kellen.schroder@somosphi.com.br>
2022-02-08 20:54:54 -0300

    created bitcoin model and service

---
+++ src/main/java/org/kellensdk/model/Bitcoin.java
@@ -0,0 +1,15 @@
+package org.kellensdk.model;
+
+import java.time.LocalDate;
+
+public class Bitcoin {
+
+    private Long id;
+
+    private Double preço;
+
+    private String tipo;
+
+    private LocalDate data;
+
+}
---
+++ src/main/java/org/kellensdk/service/BitcoinService.java
@@ -0,0 +1,10 @@
+package org.kellensdk.service;
+
+import org.kellensdk.model.Bitcoin;
+
+import java.util.List;
+
+public interface BitcoinService {
+
+    public List<Bitcoin> listar();
+}
